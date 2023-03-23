24625bf0075f0f0f86a55301e8fdfeef8f8e25ae
Abby Pidgeon <Abigail.L.Pidgeon-1@ou.edu>
2023-03-22 20:44:44 -0500

    finished practice 2

---
+++ problem 2/Program.cs
@@ -0,0 +1,31 @@
+﻿List<string> homestates = new List<string>();
+
+do
+{
+    Console.WriteLine("please enter a state you have lived in.");
+    string state = Console.ReadLine();
+    homestates.Add(state);
+
+    Console.WriteLine("Do you have another state to add yes or no?");
+
+} while (Console.ReadLine().ToLower()[0] == 'y');
+
+Console.WriteLine($"You have lived in {homestates.Count()} states.");
+
+double max = 0;
+
+foreach (string state in homestates)
+{
+    if (state.Length > max)
+    {
+        max = state.Length;
+    }
+}
+
+foreach (string state in homestates)
+{
+    if (state.Length == max)
+    {
+        Console.WriteLine($"{state.ToUpper()}-{state[0]}{state[1]}");
+    }
+}
