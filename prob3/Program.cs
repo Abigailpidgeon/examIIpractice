db7b6d24592dd4b6f2e96b3a3308b16d00c9cb09
Abby Pidgeon <Abigail.L.Pidgeon-1@ou.edu>
2023-03-22 21:20:44 -0500

    half way done with practice 3

---
+++ prob3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.");
+
+Console.WriteLine("What would you like to search for ?");
+string search = Console.ReadLine();
+
+Console.WriteLine("what word would you like to change it to");
+string change = Console.ReadLine();
+
